d2a8aa1a60ec4ad711fcf8409099c204bb826732
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-10 11:48:49 -0800

    added fifth test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,20 +3,24 @@ require('anagram')
 require('pry')
 
 describe('#anagram') do
-  it("returns true if two words are anagrams") do
-    obj1 = Anagram.new("words", "sword")
-    expect(obj1.anagram_checker).to(eq("These words are anagrams."))
+  it("checks if two words are anagrams") do
+    obj = Anagram.new("words", "sword")
+    expect(obj.anagram_checker).to(eq("These words are anagrams."))
   end
-  it("returns true if two words with different cases are anagrams") do
-    obj1 = Anagram.new("Words", "Sword")
-    expect(obj1.anagram_checker).to(eq("These words are anagrams."))
+  it("checks if two words with different cases are anagrams") do
+    obj = Anagram.new("Words", "Sword")
+    expect(obj.anagram_checker).to(eq("These words are anagrams."))
   end
-  it("returns true if two words both contain vowels") do
-    obj1 = Anagram.new("Wrds", "Swrd")
-    expect(obj1.anagram_checker).to(eq("You need to input actual words!"))
+  it("checks if two words both contain vowels") do
+    obj = Anagram.new("Wrds", "Swrd")
+    expect(obj.anagram_checker).to(eq("You need to input actual words!"))
   end
-  it("returns if the two words are antigrams") do
-    obj1 = Anagram.new("Banana", "Shoe")
-    expect(obj1.anagram_checker).to(eq("These words have no letter matches and are antigrams."))
+  it("checks if the two words are antigrams") do
+    obj = Anagram.new("Banana", "Shoe")
+    expect(obj.anagram_checker).to(eq("These words have no letter matches and are antigrams."))
+  end
+  it("checks if two sentences are anagrams or antigrams") do
+    obj = Anagram.new("The Morse Code", "Here come dots!")
+    expect(obj.anagram_checker).to(eq("These sentences are anagrams."))
   end
 end
