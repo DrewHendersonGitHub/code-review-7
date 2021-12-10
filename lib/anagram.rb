7b8c02d271085da33512882b4ae8a88b83b54ec7
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-10 11:38:39 -0800

    passed forth test, checks if they are antigrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,23 @@ class Anagram
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
+    @letters1 = @word1.downcase.split('').sort
+    @letters2 = @word2.downcase.split('').sort
   end
+  
   def anagram_checker
-    if @word1.downcase.split('').sort == @word2.downcase.split('').sort && (@word1.downcase.count 'aeiouy').to_s != '0'
-      return true
+    if @letters1 == @letters2
+      if (@word1.downcase.count 'aeiouy').to_s != '0'
+        return "These words are anagrams."
+      else
+        return "You need to input actual words!"
+      end
+    else
+      if (@word1.downcase.count @word2.downcase).to_s == '0'
+        return "These words have no letter matches and are antigrams."
+      else
+        return "These words are not anagrams."
+      end
     end
     false
   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,18 +5,18 @@ require('pry')
 describe('#anagram') do
   it("returns true if two words are anagrams") do
     obj1 = Anagram.new("words", "sword")
-    expect(obj1.anagram_checker).to(eq(true))
+    expect(obj1.anagram_checker).to(eq("These words are anagrams."))
   end
   it("returns true if two words with different cases are anagrams") do
     obj1 = Anagram.new("Words", "Sword")
-    expect(obj1.anagram_checker).to(eq(true))
+    expect(obj1.anagram_checker).to(eq("These words are anagrams."))
   end
   it("returns true if two words both contain vowels") do
     obj1 = Anagram.new("Wrds", "Swrd")
-    expect(obj1.anagram_checker).to(eq(false))
+    expect(obj1.anagram_checker).to(eq("You need to input actual words!"))
   end
-  it("returns if the two words are anitgrams") do
+  it("returns if the two words are antigrams") do
     obj1 = Anagram.new("Banana", "Shoe")
-    expect(obj1.anagram_checker).to(eq("These words are antigrams"))
+    expect(obj1.anagram_checker).to(eq("These words have no letter matches and are antigrams."))
   end
 end
