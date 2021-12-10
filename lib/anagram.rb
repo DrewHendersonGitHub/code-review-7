5086f42aff9580c849c49f162b9ec94da8b97fdc
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-10 09:33:56 -0800

    passed second test, anagram_checker ignores letter cases

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 class Anagram
   def initialize(word1, word2)
-    @letters1 = word1.split('').sort
-    @letters2 = word2.split('').sort
+    @letters1 = word1.downcase.split('').sort
+    @letters2 = word2.downcase.split('').sort
   end
   def anagram_checker
     if @letters1 == @letters2
