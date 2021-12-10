92a8ce0c5723617b2cfa6d066673e011f3d5e53e
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-10 09:31:54 -0800

    added second test for Anagram

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,4 +7,8 @@ describe('#anagram') do
     obj1 = Anagram.new("words", "sword")
     expect(obj1.anagram_checker).to(eq(true))
   end
+  it("returns true if two words with different cases are anagrams") do
+    obj1 = Anagram.new("Words", "Sword")
+    expect(obj1.anagram_checker).to(eq(true))
+  end
 end
