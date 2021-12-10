b2089a239ac5968d686de4618d6501f5394110c0
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-10 15:04:33 -0800

    added script file

--- lib/script.rb
+++ lib/script.rb
@@ -1,1 +1,15 @@
-puts "Hello World!"
+#!usr/bin/env ruby
+require('./lib/anagram.rb')
+
+puts "Enter Two Words: "
+puts "Type 'quit' to exit"
+word1 = gets.chomp
+
+while (word1!='quit')
+  word2 = gets.chomp
+  obj = Anagram.new(word1, word2)
+  puts obj.anagram_checker
+  puts "Enter 'quit' to exit, or enter two more words: "
+  word1 = gets.chomp
+end
+puts "Goodbye"
